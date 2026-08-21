008e5f9751b8131d67a17d88aa57d9eaa4a752b6
Anushka Jagat <anushkajagat06@gmail.com>
2026-08-21 14:08:50 +0530

    fix cors for production frontend

--- backend/src/main.js
+++ backend/src/main.js
@@ -11,7 +11,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: [
+      'http://localhost:3000',
+      'https://taskora0.netlify.app',
+    ],
   });
 
   await app.listen(process.env.PORT || 3004);
